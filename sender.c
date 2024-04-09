cdcfc82f86ea8201e4f7ceb67864e81ecf7ade8f
B-den <dbancre@gmail.com>
2024-04-09 19:09:55 +0300

    complete v1 sender

--- sender.c
+++ sender.c
@@ -8,15 +8,15 @@
 
 #define RETRY_TIMES 5
 #define RESEND_TIMES 3
-#define RETRY_TIMEOUT 500 // nanosecs
+#define RETRY_TIMEOUT 50000 // nanosecs     // yet to experiment; context switch takes ~1500ns
 #define BLOCK_SIZE 1024
 
 struct send_arg {
-    pthread_t tid_to_kill;
+    pthread_t* tid_to_kill;
     int success;
     int sockfd;
-    const package_t* package;
-    const struct sockaddr_un* addr;
+    package_t package;
+    struct sockaddr_un* addr;
 };
 
 void* send_with_confirm(
@@ -27,7 +27,7 @@ void* send_with_confirm(
     result_t result = { .error = 1 };
     while (result.error) {
         sendto(arg->sockfd,
-               arg->package,
+               &arg->package,
                sizeof(package_t),
                0,
                arg->addr,
@@ -40,7 +40,7 @@ void* send_with_confirm(
 }
 
 struct timeout_arg {
-    pthread_t tid_to_kill;
+    pthread_t* tid_to_kill;
     unsigned int nanosecs;
 };
 
@@ -52,14 +52,46 @@ void* timeout(void* _arg) {
     return NULL;
 }
 
-int send_data(void* data, size_t len) {
+void fill_tmp_socket_addr(struct sockaddr_un* addr) {
+    // todo (no)
+    addr->sun_family = AF_UNIX;
+    const char* tmp_name = "/tmp/usp_tmp_client_socket";
+    memcpy(addr->sun_path, tmp_name, sizeof(tmp_name));
+}
+
+int send_data(int sockfd, void* data, size_t len, struct sockaddr_un* addr /*NULL-able*/) {
     struct send_arg s_arg;
     struct timeout_arg t_arg;
     pthread_t s_tid, t_tid;
     pthread_attr_t s_attr, t_attr;
-    for (size_t offset = 0; offset < len; offset += BLOCK_SIZE) {
-        size_t actual_size = len - offset > BLOCK_SIZE? BLOCK_SIZE: len - offset;
-        // todo
+    int to_clear = 0, rc = 0, client_socket;
+    if (addr == NULL) {
+        struct sockaddr_un a;
+        addr = &a;
+        fill_tmp_socket_addr(addr);
+        client_socket = socket(AF_UNIX, SOCK_DGRAM, 0);
+        to_clear = 1;
+        unlink(addr->sun_path);
+        if (bind(client_socket, (const struct sockaddr*) addr, sizeof(*addr)) < 0) {
+            perror("binding in sender");
+            rc = 1;
+            goto exit;
+        }    
+    }
+
+    s_arg.sockfd = sockfd;
+    s_arg.addr = addr;
+    s_arg.tid_to_kill = &t_tid;
+    s_arg.package.total_size = (unsigned int)(len % BLOCK_SIZE == 0? len / BLOCK_SIZE: len / BLOCK_SIZE + 1);
+
+    t_arg.nanosecs = RETRY_TIMEOUT;
+    t_arg.tid_to_kill = &s_tid;
+
+    for (size_t num = 0; num * BLOCK_SIZE < len; num++) {
+        size_t actual_size = len - num * BLOCK_SIZE > BLOCK_SIZE? BLOCK_SIZE: len - num * BLOCK_SIZE;
+        memcpy(s_arg.package.datagram.data, (char*)data + num * BLOCK_SIZE, actual_size);
+        s_arg.package.order_number = num;
+
         int resend_counter = 0;
         for (int i = 0; i < RETRY_TIMES; i++) {
             s_arg.success = 0;
@@ -71,8 +103,14 @@ int send_data(void* data, size_t len) {
             }
         }
         if (!s_arg.success) {
-            return 1;
+            rc = 1;
+            goto exit;
         }
     }
-    return 0;
+exit:
+    if (to_clear) {
+        close(client_socket);
+        unlink(addr->sun_path);
+    }
+    return rc;
 }
