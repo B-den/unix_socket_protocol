a7ad35d251ceb0d67851550d3de79daaaef6a649
Schlypar <schlypar@gmail.com>
2024-04-11 00:16:00 +0300

    removed struct s_arg for simplicity

--- send.c
+++ send.c
@@ -12,13 +12,6 @@
 #include "checksum.h"
 #include "packet.h"
 
-struct send_arg {
-    int error;
-    int sockfd;
-    package_t package;
-    struct sockaddr_un* addr;
-};
-
 void fill_tmp_client_socket_addr(struct sockaddr_un* addr) {
     // todo (no)
     addr->sun_family = AF_UNIX;
@@ -38,7 +31,9 @@ int send_data(
         return 1;
     }
 
-    struct send_arg s_arg;
+    int error;
+    package_t package;
+
     fd_set file_descriptor_set;
     struct timeval timeout = { 0, RETRY_TIMEOUT }; // in microsecs
 
@@ -67,30 +62,28 @@ int send_data(
     bzero(&addr, sizeof(addr));
     memcpy(addr.sun_path, target_socket_filename, strlen(target_socket_filename) + 1);
     addr.sun_family = AF_UNIX;
-    s_arg.sockfd = sockfd;
-    s_arg.addr = &addr;
-    s_arg.package.total_size = (unsigned int) (len % BLOCK_SIZE == 0 ? len / BLOCK_SIZE : len / BLOCK_SIZE + 1);
+    package.total_size = (unsigned int) (len % BLOCK_SIZE == 0 ? len / BLOCK_SIZE : len / BLOCK_SIZE + 1);
 
     for (size_t num = 0; num * BLOCK_SIZE < len; num++) {
         size_t actual_size = len - num * BLOCK_SIZE > BLOCK_SIZE
                 ? BLOCK_SIZE
                 : len - num * BLOCK_SIZE;
-        memcpy(s_arg.package.datagram.data,
+        memcpy(package.datagram.data,
                (char*) data + num * BLOCK_SIZE,
                actual_size);
-        s_arg.package.order_number = num + 1;
-        s_arg.package.datagram.checksum = checksum(0, s_arg.package.datagram.data, actual_size);
-        s_arg.package.datagram.len = actual_size;
+        package.order_number = num + 1;
+        package.datagram.checksum = checksum(0, package.datagram.data, actual_size);
+        package.datagram.len = actual_size;
 
         int resend_counter = 0;
-        s_arg.error = 1;
-        for (int i = 0; i < RETRY_TIMES && s_arg.error; i++) {
-            sendto(s_arg.sockfd,
-                   &s_arg.package,
+        error = 1;
+        for (int i = 0; i < RETRY_TIMES && error; i++) {
+            sendto(sockfd,
+                   &package,
                    sizeof(package_t),
                    0,
-                   (struct sockaddr*) s_arg.addr,
-                   sizeof(*s_arg.addr));
+                   (struct sockaddr*) &addr,
+                   sizeof(addr));
 
             timeout.tv_sec = 0;
             timeout.tv_usec = RETRY_TIMEOUT;
@@ -104,16 +97,16 @@ int send_data(
                 fprintf(stderr, "receive timeout expired\n");
                 continue;
             }
-            recvfrom(s_arg.sockfd, &s_arg.error, sizeof(s_arg.error), 0, (struct sockaddr*) s_arg.addr, NULL);
-            if (s_arg.error && resend_counter < RESEND_TIMES) {
+            recvfrom(sockfd, &error, sizeof(error), 0, (struct sockaddr*) &addr, NULL);
+            if (error && resend_counter < RESEND_TIMES) {
                 i--;
                 resend_counter++;
             } else {
                 break;
             }
         }
-        if (s_arg.error) {
-            perror("s_arg.error == 1");
+        if (error) {
+            perror("error == 1");
             rc = 1;
             goto exit;
         }
